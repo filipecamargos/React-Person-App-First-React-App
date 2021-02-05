efae8a9cfd78e9500c33f407e42472360a8f66ed
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-05 13:57:54 -0700

    Props and State

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ import Person from './Person/Person' //inport from Person the function person
 //This is a react component
 //Every react component render (HTML -> (EJX)) to the DOM
 class App extends Component {
+    //state only avaliable in a class where it extends Componets obs: There are hooks
+    //States allow to use data in this case you can store things that you need to manipulate in this class
+    //When state changes it leads React to update the DOM
+    state = {
+      persons: [
+        { name:"Bob", age:"30"},
+        { name:"Luke", age:"33"},
+        { name:"Mark", age:"37"},
+      ],
+    }
+
+
     render() {
         return (
           <div className ="App">
             <h1> Hi this is my first React App </h1>
-            <Person name="Bob" age="30" />
-            <Person name="Luke" age="33" >I like to code</Person>
-            <Person name="Mark" age="37" />
+            <button>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like to code!</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
           </div>
         );
         
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,9 +4,13 @@ import React from 'react'
 
 //A component can be just a normal a function that return JSX
 const person = (props) => { //Pros are the values that are passed in the Person tag where it is imported
+   //{} Curly braces take the data passed in the tag ex: <Person name="x" age="x" />
+   //Children represent take the what is passed within the tag ex: <Person> some text </Person>
     return (
-        //Single curly braces allows you to process data and have dinamic content
+        <div>
         <p>I'm {props.name}! I am {props.age} years old!</p>
+        <p>{props.children}</p>
+        </div>
     );
 }
 
