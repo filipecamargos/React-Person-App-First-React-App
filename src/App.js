d527354082431a059ebf8e0afe67e2a09bc166f1
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-06 16:01:59 -0700

    Change back into Class Based

--- src/App.js
+++ src/App.js
@@ -5,28 +5,55 @@ import './App.css';
 //User upercase for the your elements components beacause the lower are default for the HTML
 import Person from './Person/Person' //inport from Person the function person
 
+
+
+//Export the class as default 
+export default app;
+
+
+
+/*###################################################################
+Everything below was done in as state hooks which we converted back
+into class based 
+
 //This is a react component
 //Every react component render (HTML -> (EJX)) to the DOM
 const app = props =>  {  //class App extends Component got converted into rooks
 
   //Call state to use it it will return an array with two elements
-  const [personState, setPerson] = useState({  
+  const [personState, setPersonState] = useState({  
     //state only avaliable in a class where it extends Componets obs: There are hooks
     //States allow to use data in this case you can store things that you need to manipulate in this class
     //When state changes it leads React to update the DOM
-    state = {
-      persons: [
+    //state = {
+    persons: [
         { name:"Bob", age:"30"},
         { name:"Luke", age:"33"},
         { name:"Mark", age:"37"},
       ],
-    }
+    //}
   })
+
+  //The onClick button will handle this funtion that change the names
+  const switchNameHandler = () => {
+    //Don't Change state directly: this.state.persons[0].name = "Bob Marley";
+    //Setters allows you to overide the other state
+    setPersonState({ //Set states will always overwrite the older state, so you have to make sure to copy the older stuff
+      persons: [
+      { name:"Bob Marley", age:"50"},
+      { name:"Luke", age:"33"},
+      { name:"Mark", age:"40"},
+    ]}
+    )
+  }; 
+  
+  //Note: you can have multiples uses states so you can manager it ex:
+  // const [addressState, setAdressState] = useState ('Some Dress')
 
   return (
     <div className ="App">
       <h1> Hi this is my first React App </h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <button onClick={switchNameHandler}>Switch Name</button>
       <Person 
         name={personState.persons[0].name} 
         age={personState.persons[0].age}
@@ -46,22 +73,5 @@ const app = props =>  {  //class App extends Component got converted into rooks
   //This is what JSX gets compiled to -> Create takes three element, JS object for class or id and text
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React APP!!!'));
 
-}
-
-//Export the class as default 
-export default app;
-
-
-
-        //The onClick button will handle this funtion that change the names
-        switchNameHandler = () => {
-          //Don't Change state directly: this.state.persons[0].name = "Bob Marley";
-          //Setters allows you to overide the other state
-          this.setState({
-            persons: [
-            { name:"Bob Marley", age:"50"},
-            { name:"Luke", age:"33"},
-            { name:"Mark", age:"40"},
-          ]}
-          )
-        };
+} 
+*/
